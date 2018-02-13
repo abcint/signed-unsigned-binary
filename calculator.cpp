b94845f8442cfa719f137b5d9a0d1f5de0491c52
brs80 <brs80@zips.uakron.edu>
2018-02-13 14:08:08 -0500

    changed prompt to unsigned/signed

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,6 @@
 #include <string> 
-#include <iostream> 
+#include <iostream>
+#include <cmath> 
 
 
 int getBinary(std::string s) {
@@ -13,12 +14,15 @@ int main() {
 	std::string sign = ""; 
 	std::cout << "Enter the binary string: ";
 	std::cin >> binary;
-	std::cout << "is it positive/negative (0/1)(p/n): "; 
+	std::cout << "is it signed/unsigned (0/1)(s/u): "; 
 	std::cin >> sign;
  	// throw a condition here depending on pos/neg"
-	if(sign == "0" || sign == "p" || sign == "positive") { 
+	if(sign == "0" || sign == "s" || sign == "signed") { 
+		for(int i=0; i < binary.size(); i++) { 
+			std::cout << binary[i];
+		}  
+	} else { 
 		int dec = getBinary(binary);
 		std::cout << dec << "\n"; 
 	} 
-
 }  
