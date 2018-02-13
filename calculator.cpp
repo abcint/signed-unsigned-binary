5e2c22b45acb42d716eea1ab205bef2907f41fbc
brs80 <brs80@zips.uakron.edu>
2018-02-13 00:03:19 -0500

    prompt user for number

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,12 @@
-#include <iostream> 
+#include<iostream> 
+
+std::string getBinary(std::string s) {
+        return "1";
+}
+struct binary { }; 
 
 int main() { 
+	std::cout << "Enter the binary string: "; 
+	std::string binary = getBinary("10101");
 
-
-} 
-
+}  
