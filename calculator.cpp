5f7bd591e243c32dc3c202b81ccdae0b8fb3d2ad
brs80 <brs80@zips.uakron.edu>
2018-02-14 01:38:26 -0500

    input checking

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,16 @@
 /*
-** Calculator.cpp - a binary to integer converter for signed/unsigned bits (MAX 32 bits) 
+** Calculator.cpp - a binary to integer converter for signed/unsigned bits - max 32 bit
 ** Bradley Snyder brs80@zips.uakron.edu
 */
 
 #include <string>
+#include <sstream>
 #include <iostream>
 #include <cmath>
 
 
 int getBinary(std::string s) {
-	return std::stoi(s, nullptr, 2); 
+	return std::stoi(s, nullptr, 2);
 }
 
 int main() {
@@ -17,41 +18,41 @@ int main() {
 	std::string binary = "", sign = "";
 	int decimal = 0;
 
-	std::cout << "\n\n" << "Welcome to the binary->decimal calculator!" << "\n"; 
-	std::cout << "Enter \"x\" for binary value to exit" << "\n\n"; 
+	std::cout << "\n\n" << "welcome to a binary-to-decimal convertor!" << "\n"; 
 
 	while(binary != "X" && binary != "x" && binary != "exit" && binary != "Exit") { 
-		std::cout << "Enter the binary value: ";
-		std::cin >> binary;
-		std::cout << "is it (signed/unsigned) (s/u): ";
-		std::cin >> sign;
+		decimal = 0;
+		std::cout << "Binary twos representation: ";
+		getline(std::cin, binary);
+		if(binary.empty()) break;
 
-		decimal = 0; 
-		if(sign == "0" || sign == "s" || sign == "signed") {
+		std::cout << "(signed/unsigned) (s/u): ";
+		getline(std::cin, sign);
+		if(sign.empty()) sign = "u";
+
+		if(sign == "Signed" || sign == "signed" || sign == "0" || sign == "s") {
 			if(binary[0] == '0') {
 				for(int i=1; i<binary.size(); i++) {
-					int temp = binary[i] - '0'; 
-					decimal += temp * pow(2.0 , binary.size()-i-1);
+					decimal += (binary[i]-'0') * pow(2.0 , binary.size()-i-1); // xn-1 * 2^n-1 ...-> xi * 2i
 				}
-				std::cout << decimal << "\n"; 
+				std::cout << decimal << "\n";
 			}
-			if(binary[0] == '1') { 
-				for(int i=1; i<binary.size(); i++) { 
+			if(binary[0] == '1') {
+				for(int i=1; i<binary.size(); i++) {
 					int temp = binary[i] - '0';
-					temp == 0 ? temp=1 : temp=0;
+					temp == 0 ? temp=1 : temp=0; // changing all the 0s to 1s
 					decimal += temp * pow(2.0 , binary.size()-i-1);
 				}
 				decimal = -decimal-1;
-				std::cout << "-----------------------------------------------------------------------------" << "\n"; 
-				std::cout << "signed binary value -> " << binary << "\n" << "decimal value-> " << decimal << "\n"; 
-				std::cout << "-----------------------------------------------------------------------------" << "\n\n"; 
+				std::cout << "-----------------------------------------------------------------------------" << "\n";
+				std::cout << "signed binary value -> " << binary << "\n" << "decimal value-> " << decimal << "\n";
+				std::cout << "-----------------------------------------------------------------------------" << "\n\n";
 			}
-		} else {
+		} else if(sign == "u" ||  sign == "unsigned") {
 			int dec = getBinary(binary);
-			std::cout << "-----------------------------------------------------------------------------" << "\n"; 
+			std::cout << "-----------------------------------------------------------------------------" << "\n";
 			std::cout << "unsigned binary value -> " << binary << "\n" << "decimal value -> " <<  dec << "\n";
-			std::cout << "-----------------------------------------------------------------------------" << "\n\n"; 
-
-		}
+			std::cout << "-----------------------------------------------------------------------------" << "\n\n";
+		} else break;
 	}
 }
